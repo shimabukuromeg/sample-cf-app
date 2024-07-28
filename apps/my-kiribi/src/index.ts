2376aa287d6ef248e857bb96ae22cb3789c3340f
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2024-07-28 21:39:07 +0900

    feat(my-kiribi): add client
    https://kiribi.pages.dev/client.html

--- apps/my-kiribi/src/index.ts
+++ apps/my-kiribi/src/index.ts
@@ -1,8 +1,14 @@
 import { Kiribi } from 'kiribi';
 import { KiribiPerformer } from 'kiribi/performer';
+import { client } from 'kiribi/client';
+import { rest } from 'kiribi/rest';
 
-export default class extends Kiribi {}
+export default class extends Kiribi {
+	client = client;
+	rest = rest;
+}
 
+// Jobの定義
 export class MyPerformer extends KiribiPerformer {
 	async perform(payload: any) {
 		// Do something with the payload
